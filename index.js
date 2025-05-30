5ca1d3639bff75fc3e96b157bdf2cdc8e9ab5483
community-vercel <community-vercel>
2025-05-30 07:03:15 -0700

    AA

--- index.js
+++ index.js
@@ -71,9 +71,9 @@ app.use(cors({
       "http://localhost:5000",
       "http://localhost",
       "http://localhost:8000",
-        "https://custom-gpt-backend-sigma.vercel.app",
-        "https://admin-customchatbot-app.vercel.app",
-
+      "https://custom-gpt-backend-sigma.vercel.app",
+      "https://admin-customchatbot-app.vercel.app",
+      "https://custom-gpt-builder-frontend.vercel.app/",
       "https://accounts.google.com", // Allow Google's OAuth domain for redirects
 
     ];

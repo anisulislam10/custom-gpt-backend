3d32c738a41fb7be0f91037fada072d8d7b1fa4e
community-vercel <community-vercel>
2025-06-30 12:50:20 -0700

    aaa

--- index.js
+++ index.js
@@ -67,6 +67,7 @@ app.use(cors({
     const allowedOrigins = [
       process.env.FRONTEND_URL,
       "http://localhost:3000",
+      "http://165.227.120.144",
       "http://localhost:3001",
       "http://localhost:5000",
       "http://localhost",

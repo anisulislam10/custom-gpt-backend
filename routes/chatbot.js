e58b5a08bffe75a0f6f15a128db949524a12f272
community-vercel <community-vercel>
2025-07-11 04:45:23 -0700

    aaa

--- routes/chatbot.js
+++ routes/chatbot.js
@@ -6,6 +6,10 @@ const Interaction = require('../models/Interaction');
 const FormResponse = require('../models/FormResponse');
 const { v4: uuidv4 } = require('uuid');
 const geoip = require('geoip-lite'); // Import geoip-lite
+const express = require('express');
+const app = express();
+
+app.set('trust proxy', true);
 
 // GET /:flowId/:userId - Serve the chatbot HTML
 router.get('/:flowId/:userId', async (req, res) => {
